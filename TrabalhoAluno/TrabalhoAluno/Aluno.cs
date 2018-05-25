17f11fecde007a5e8591c22b58d034ea64c9b1c2
hakkai1641 <souzadyego2@gmail.com>
2018-05-25 20:03:45 -0300

    Add files via upload

---
+++ TrabalhoAluno/TrabalhoAluno/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoAluno
+{
+    class Aluno
+    {
+        public String nome { get; set; }
+        public DateTime data_nascimento { get; set; }
+        public String Matricula_Aluno { get; set; }
+
+    }
+}
